35886b8b99e93355c6966975d56a4abbbb2576c7
aishw <bhaleraoaishwarya8@gmail.com>
2025-06-19 07:55:26 +0530

    Challenge1

---
+++ ex_11_Java_Basic/Test_Challenge_001.java
@@ -0,0 +1,23 @@
+package ex_11_Java_Basic;
+
+public class Test_Challenge_001 {
+        public static void main(String[] args) {
+            //Write a Java program that demonstrates proper variable declaration and initialization for different data types.
+//**Requirements:**
+//- Declare and initialize variables of different primitive data types (int, double, boolean, char)
+//- Print the values of all variables
+//- Show proper naming conventions
+            int a = 25;
+            double b= 3.14;
+            boolean c= true;
+            char d='A';
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+
+
+
+        }
+    }
+
