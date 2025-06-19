5ba43afd561ac3cfe9943c54eba523c3ea0494bb
aishw <bhaleraoaishwarya8@gmail.com>
2025-06-19 08:04:18 +0530

    Challenge8

---
+++ ex_11_Java_Basic/Test_Challenge_008.java
@@ -0,0 +1,26 @@
+package ex_11_Java_Basic;
+
+import java.util.Scanner;
+
+public class Test_Challenge_008 {
+    public static void main(String[] args) {
+        System.out.println("Enter the year");
+        Scanner sc =new Scanner(System.in);
+        int year=sc.nextInt();
+        if(year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + "Year is a leap year");
+                } else {
+                    System.out.println("Year is not leap");
+                }
+            } else {
+                System.out.println("Year is a leap year");
+            }
+        }
+        else
+        {
+            System.out.println("Year is not leap");
+        }
+    }
+}
