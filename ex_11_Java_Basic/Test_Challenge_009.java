3437f6e2b7c8d2b141604b94932ca010df5d2839
Compass-automation <138752978+Compass-automation@users.noreply.github.com>
2025-06-22 00:11:55 +0530

    Challenge9

---
+++ ex_11_Java_Basic/Test_Challenge_009.java
@@ -0,0 +1,40 @@
+package ex_11_Java_Basic;
+
+import java.util.Scanner;
+
+public class Test_Challenge_009 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number betweeen 1 to 7 ");
+        int day = sc.nextInt();
+        switch (day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid Day Number");
+        }
+
+
+
+    }
+}
